ab2712f58397a057b0dde5b98d6e8ccbde4c98a2
James Pratt <me@jamiep.org>
2014-05-21 15:56:17 +0700

    fix notice about $timeformat

--- type/shipmate/certificate_base.php
+++ type/shipmate/certificate_base.php
@@ -5,6 +5,9 @@ if (!defined('MOODLE_INTERNAL')) {
 //Create new PDF document
 
 $pdf = new TCPDF('L', 'pt', 'Letter', true, 'UTF-8', false);
+
+$timeformat = get_string('strftimedaydate');
+
 //$pdf->SetProtection(array('print'));
 $pdf->SetTitle($certificate->name);
 $pdf->setPrintHeader(false);
@@ -14,9 +17,9 @@ $pdf->AddPage();
 $pdf->SetAutoPageBreak(false, 0);
 
 // Add images and lines
-$pdf->Image("$CFG->dirroot/mod/certificate/pix/borders/shipmate2.jpg", 10, 12, 770, 588); 
+$pdf->Image("$CFG->dirroot/mod/certificate/pix/borders/shipmate2.jpg", 10, 12, 770, 588);
 $pdf->Image("$CFG->dirroot/mod/certificate/pix/watermarks/certificate.jpg", 85, 120);
-certificate_print_image($pdf, $certificate, CERT_IMAGE_SIGNATURE, 320, 430, '', '');  
+certificate_print_image($pdf, $certificate, CERT_IMAGE_SIGNATURE, 320, 430, '', '');
 
 $pdf->SetLineWidth(1);
 $pdf->Line(320, 483, 460, 483); //sigsize 140 * 63
@@ -31,14 +34,14 @@ $pdf->setFont("freeserif", "I", 12);
 $pdf->Cell(200, 14, 'Developed and Authored by:', 0, 'J');
 $pdf->SetXY(540, 406);
 $pdf->setFont("freeserif", "B", 12);
-$pdf->Cell(200, 14, 'ShipMate, Inc.', 0, 'J');  
+$pdf->Cell(200, 14, 'ShipMate, Inc.', 0, 'J');
 
 $pdf->setFont("freeserif", "", 12);
 $address = "780 Buckaroo Trail, Suite D\nSisters, OR 97759-0787\n".
         "Tel: +1 (310) 370-3600\nFax: +1 (310) 370-5700\nhttp://www.shipmate.com/";
 
 $pdf->SetXY(540, 420);
-$pdf->MultiCell(200, 14, $address, 0, 'J'); 
+$pdf->MultiCell(200, 14, $address, 0, 'J');
 
 // Add text
 $pdf->Image("$CFG->dirroot/mod/certificate/pix/title.png", 115, 70, 0, 0);
@@ -47,7 +50,7 @@ $pdf->SetTextColor(0,0,0);
 
 $pdf->SetXY(75, 158);
 $pdf->setFont("freeserif", "", 28);
-$pdf->MultiCell(640, 30, $course->fullname, 0, 'C');  
+$pdf->MultiCell(640, 30, $course->fullname, 0, 'C');
 
 certificate_print_text($pdf, 35, 230, 'C', 'freeserif', 'I', 20, 'presented to');
 certificate_print_text($pdf, 35, 260, 'C', 'freeserif', 'B', 30, fullname($USER));
